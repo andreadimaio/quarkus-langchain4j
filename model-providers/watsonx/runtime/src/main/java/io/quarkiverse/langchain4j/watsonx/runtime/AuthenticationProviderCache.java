4831b680a5ac3fe5f8eaa8535b46e48d3990fb56
andreadimaio <dmaio.andrea@gmail.com>
2025-09-30 15:42:06 +0200

    WIP

--- model-providers/watsonx/runtime/src/main/java/io/quarkiverse/langchain4j/watsonx/runtime/AuthenticationProviderCache.java
+++ model-providers/watsonx/runtime/src/main/java/io/quarkiverse/langchain4j/watsonx/runtime/AuthenticationProviderCache.java
@@ -2,23 +2,20 @@ package io.quarkiverse.langchain4j.watsonx.runtime;
 
 import java.net.URI;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 import com.ibm.watsonx.ai.core.auth.AuthenticationProvider;
 import com.ibm.watsonx.ai.core.auth.iam.IAMAuthenticator;
 
-// TODO: TO REMOVE
 public class AuthenticationProviderCache {
 
     private static final Map<String, AuthenticationProvider> cache = new ConcurrentHashMap<>();
 
-    public static Optional<AuthenticationProvider> get(String apiKey) {
-        return Optional.ofNullable(cache.get(apiKey));
+    private AuthenticationProviderCache() {
     }
 
-    public static AuthenticationProvider getOrCreateTokenGenerator(URI baseUrl, String apiKey) {
+    static AuthenticationProvider getOrCreateTokenGenerator(URI baseUrl, String apiKey) {
         return cache.computeIfAbsent(apiKey,
                 new Function<String, AuthenticationProvider>() {
                     @Override
